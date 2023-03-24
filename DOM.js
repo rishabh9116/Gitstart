d4204c90a5bdb017267535d9adc83425460170ce
rishabh9116 <rishabhdwivedi462@gmail.com>
2023-03-24 15:04:27 +0530

    adding html files

---
+++ DOM.js
@@ -0,0 +1,18 @@
+console.dir(document);
+console.dir(document.domain);
+console.log(document.URL);
+console.dir(document.title);
+document.title= 'Rishbah' ;
+console.dir(document.title);  
+console.log(document.doctype);  
+console.log(document.head);  
+console.log(document.body);  
+console.log(document.all);  
+ console.log(document.all[10]);  
+document.all[10].textContent = 'Hello';
+console.log(document.forms);
+console.log(document.links);
+console.log(document.images);   
+console.log(document.getElementById('header-title'));
+var hT = document.getElementById('header-title');
+console.log(hT);
