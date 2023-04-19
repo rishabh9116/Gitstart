28fc363f5b1a7bda15087581169df3602929cea2
rishabh9116 <rishabhdwivedi462@gmail.com>
2023-04-19 07:40:38 +0530

    applied useState hook

--- React/expenseitem/src/Components/Expenses/ExpenseItem.js
+++ React/expenseitem/src/Components/Expenses/ExpenseItem.js
@@ -1,24 +1,36 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './ExpenseItem.css'
 import Card from '../UI/Card'
 import ExpenseDate from './ExpenseDate.js'
 import ExpenseDetails from './ExpenseDetails'
+
+
 const ExpenseItem = (props) => {
+  const [title,setTitle]  = useState(props.title);
+  const [amount,setAmount] = useState(props.amount);
+  
 const clickHandler = () => {
+  setTitle('Updated')
   console.log('Clicked!!!!');
 }
 const deleteHandler = (event) =>{
   
+}
+
+function changeAmountHandler() {
+  setAmount(100);
 }
   return (
     <Card className='expense-item'>
 
      <ExpenseDate date ={props.date} />
 
-     <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
-     
+     {/* <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} /> */}
+    <div >{title}</div> 
+    <div>${amount}</div>
      <button onClick={clickHandler}>Change Title</button>
      <button onClick={deleteHandler}>Delete Expense</button>
+     <button onClick={changeAmountHandler}>Change amount</button>
   </Card>
   )
 }
