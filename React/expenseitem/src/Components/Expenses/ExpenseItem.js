da88d95fe4a612d7b758cad8c757087342bd816f
rishabh9116 <rishabhdwivedi462@gmail.com>
2023-04-19 07:00:24 +0530

    Add eventListener

--- React/expenseitem/src/Components/Expenses/ExpenseItem.js
+++ React/expenseitem/src/Components/Expenses/ExpenseItem.js
@@ -4,15 +4,21 @@ import Card from '../UI/Card'
 import ExpenseDate from './ExpenseDate.js'
 import ExpenseDetails from './ExpenseDetails'
 const ExpenseItem = (props) => {
-
-
+const clickHandler = () => {
+  console.log('Clicked!!!!');
+}
+const deleteHandler = (event) =>{
+  
+}
   return (
     <Card className='expense-item'>
 
      <ExpenseDate date ={props.date} />
 
      <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
-
+     
+     <button onClick={clickHandler}>Change Title</button>
+     <button onClick={deleteHandler}>Delete Expense</button>
   </Card>
   )
 }
