27de0480b5398b583e09ddc2326814c8ac1729a8
rishabh9116 <rishabhdwivedi462@gmail.com>
2023-09-14 00:20:05 +0530

    apply retry strategy

--- React/MovieProject/01-starting-project/src/App.js
+++ React/MovieProject/01-starting-project/src/App.js
@@ -3,34 +3,63 @@ import React, { useEffect, useState } from "react";
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 import Spinner from "./components/Spinner";
-
+var  currInterval;
 function App() {
- 
   const [moviesArr, setMoviesArr] = useState([]);
-  const[isLoader,setIsLoader] = useState(false);
+  const [isLoader, setIsLoader] = useState(false);
+  const [err, setErr] = useState(null);
+  const[intervalStopped,setIntervalStopped] = useState(false);
 
-  const fetchHandler = async() => {
+  const fetchHandler = async () => {
     setIsLoader(true);
-  const response =   await fetch("https://swapi.dev/api/films")
-  const data = await response.json();  
-  setMoviesArr(data.results);
-      setIsLoader(false);
+    setErr(null);
+    try {
+      let response = await fetch("https://swapi.dev/api/film");
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+      const data = await response.json();
+      setMoviesArr(data.results);
+    } catch (error) {
+      setErr(error.message);
+    }
+ 
+  };
+
+  const retryCall = async() => {
+    try{
+      const response = await fetch("https://swapi.dev/api/film");
+      if(!response.ok){
+        throw new Error("Something went wrong ...retrying");
+      }
+    } catch(error){
+      setErr(error.message);
+    } 
   };
  
- 
+  useEffect(() => {
+   
+    if (err) {
+      currInterval = setInterval(retryCall, 1000);
+    }
   
+  }, [err]);
+
+  const retryHandler = () => {
+    clearInterval(currInterval);
+    setIsLoader(false);
+   setIntervalStopped(true);
+  };
+
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchHandler}>Fetch Movies</button>
+        <button onClick={retryHandler}>Cancel retrying</button>
       </section>
       <section>
-        {
-          isLoader &&  <p>Loading...</p>
-        }
-       
-          
-        
+        {isLoader && <Spinner />}
+        {!intervalStopped ? (err && <p>{err}</p>):(<p>Retrying Stopped</p>)}
         <MoviesList movies={moviesArr} />
       </section>
     </React.Fragment>
